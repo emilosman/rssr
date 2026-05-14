4ba00703b56af9715e0dbb1657c306602bde882d
Emil Oszmanbegovity <emil@emilosman.com>
2026-05-14 15:50:45 +0200

    Make colors tty safe

--- internal/tui/styles.go
+++ internal/tui/styles.go
@@ -4,55 +4,54 @@ import "github.com/charmbracelet/lipgloss"
 
 var (
 	unreadStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#00cf42"))
+		Foreground(lipgloss.Color("2")) // green
 
 	bookmarkStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#FFFF00"))
+		Foreground(lipgloss.Color("11")) // yellow
 
 	errorStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#e53636"))
+		Foreground(lipgloss.Color("9")) // red
 
 	titleStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#ff4fff")).
-			Margin(0, 0, 0, 1)
+		Foreground(lipgloss.Color("13")). // magenta
+		Margin(0, 0, 0, 1)
 
 	itemTitleStyle = lipgloss.NewStyle().
-			Bold(true).
-			Foreground(lipgloss.Color("#ff4fff")).
-			Margin(0, 0, 0, 1)
+		Bold(true).
+		Foreground(lipgloss.Color("13")).
+		Margin(0, 0, 0, 1)
 
 	bookmarkItemTitleStyle = lipgloss.NewStyle().
-				Bold(true).
-				Foreground(lipgloss.Color("#FFFF00")).
-				Margin(0, 0, 0, 1)
+		Bold(true).
+		Foreground(lipgloss.Color("11")).
+		Margin(0, 0, 0, 1)
 
 	unreadItemTitleStyle = lipgloss.NewStyle().
-				Bold(true).
-				Foreground(lipgloss.Color("#00cf42")).
-				Margin(1, 0, 0, 1)
+		Bold(true).
+		Foreground(lipgloss.Color("2")).
+		Margin(1, 0, 0, 1)
 
 	statusStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("#767676")).
-			Margin(0, 0, 0, 1)
+		Foreground(lipgloss.Color("8")) // gray
 
 	contentStyle = lipgloss.NewStyle().
-			Margin(0, 0, 0, 1)
+		Margin(0, 0, 0, 1)
 
 	helpStyle = lipgloss.NewStyle().
-			Margin(0, 0, 0, 2)
+		Margin(0, 0, 0, 2)
 
 	activeTabStyle = lipgloss.NewStyle().
-			Bold(true).
-			Underline(true).
-			Foreground(lipgloss.Color("#ff4fff")).
-			Margin(0, 1, 0, 1)
+		Bold(true).
+		Underline(true).
+		Foreground(lipgloss.Color("13")).
+		Margin(0, 1, 0, 1)
 
 	unreadTabStyle = lipgloss.NewStyle().
-			Bold(true).
-			Foreground(lipgloss.Color("#00cf42")).
-			Margin(0, 1, 0, 1)
+		Bold(true).
+		Foreground(lipgloss.Color("2")).
+		Margin(0, 1, 0, 1)
 
 	inactiveTabStyle = lipgloss.NewStyle().
-				Foreground(lipgloss.Color("#dfdfdf")).
-				Margin(0, 1, 0, 1)
+		Foreground(lipgloss.Color("7")). // light gray/white
+		Margin(0, 1, 0, 1)
 )
